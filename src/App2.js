41550694f1d180060c6bb6f0e7b709db35cf5e10
Ateş Gökçeoğlu <atesgokceoglu@gmail.com>
2023-02-19 14:59:40 +0300

    Update App2.js

--- src/App2.js
+++ src/App2.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SignIn from './Components/SignIn';
+import SignUpPage from './Components/SignUpPage';
+import { useHistory } from 'react-router-dom';
 
 
 const App2 = () => {
-  return (
-    <BrowserRouter>
-        <SignIn/>
-    </BrowserRouter>
-  )
+    const Func = ()=>{
+        useHistory().push("/SignIn")
+    }
+    Func()
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path={"/SignIn"} component={SignIn} />
+                <Route path={"/SignUp"} component={SignUpPage} />
+            </Switch>
+        </BrowserRouter>
+    )
 }
 
 export default App2
