f6433242a15a89273dd7d36ec58f4675e5c10c50
Ateş Gökçeoğlu <atesgokceoglu@gmail.com>
2023-02-19 14:48:52 +0300

    Add files via upload

---
+++ App2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import SignIn from './Components/SignIn';
+
+
+const App2 = () => {
+  return (
+    <BrowserRouter>
+        <SignIn/>
+    </BrowserRouter>
+  )
+}
+
+export default App2
